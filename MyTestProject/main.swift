cc16de030defa927d6789aeba2cf95a800963027
llChintanll <www.chintanladani121@gmail.com>
2017-05-27 10:01:25 +0530

    Initial Commit

---
+++ MyTestProject/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  MyTestProject
+//
+//  Created by Administrator on 5/27/17.
+//  Copyright © 2017 zerones. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
